71f53a0f29ab43f1b0dbb66a7339dcbd6eab384b
Jessica Smith <12jessicasmith34@gmail.com>
2025-05-29 14:57:44 -0500

    create pattern validator that has optional semantic validation method and has friendly name

--- src/quartz_cron_checker/validators.py
+++ src/quartz_cron_checker/validators.py
@@ -1,10 +1,114 @@
 import re
-from collections.abc import Sequence
+from collections.abc import Callable, Sequence
+from dataclasses import dataclass
 from logging import getLogger
 
+from .constants import (
+    DOW_OCCURRENCE_PATTERN,
+    DOW_RANGE_PATTERN,
+    INCREMENT_PATTERN,
+    LAST_DOW_IN_MONTH_PATTERN,
+    LAST_N_DAYS_OF_MONTH_PATTERN,
+    LIST_OF_DIGITS_PATTERN,
+    LIST_OF_STRINGS_PATTERN,
+    MONTH_RANGE_PATTERN,
+    NEAREST_WEEKDAY_DAY_OF_MONTH_PATTERN,
+    RANGE_PATTERN,
+    RANGE_WITH_INCREMENT_PATTERN,
+    SINGLE_LITERAL_PATTERN,
+    SPECIFIC_DOW_PATTERN,
+    SPECIFIC_MONTH_PATTERN,
+    WILDCARD_PATTERN,
+)
 from .exceptions import InvalidCronStructureError
 
 LOGGER = getLogger(__name__)
+
+
+@dataclass
+class PatternValidator:
+    """Handle validation of a cron part against a regex pattern and a validator function."""
+
+    description: str
+    pattern: re.Pattern
+    validator: Callable[[str], bool] | None = None
+    failure_hint: str | None = None
+
+    def validate(self, part: str) -> tuple[bool, bool]:
+        """Validate a part against the pattern and optional validator.
+
+        Args:
+            part (str): The cron field to validate.
+
+        Returns:
+            tuple[bool,bool]: A tuple where the first element indicates if the part matches the pattern,
+            and the second element indicates if the part passes the optional validator.
+        """
+        if not self.pattern.match(part):
+            LOGGER.debug(
+                "Pattern for %r ('%s') did not match for part: %r", self.description, self.pattern.pattern, part
+            )
+            return False, False
+
+        LOGGER.debug("Pattern for %r ('%s') matched for part: %r", self.description, self.pattern.pattern, part)
+
+        if self.validator is None:
+            LOGGER.debug("No validator for %r, returning True", self.description)
+            return True, True
+
+        match = self.pattern.match(part)
+        assert match is not None, "Pattern match should not return None"
+
+        matched_value = match.groups()[0]
+        LOGGER.debug("Extracted value from part %r: %r", part, matched_value)
+
+        if self.validator is None or self.validator(matched_value):
+            return True, True
+
+        LOGGER.debug("Validator for %r failed for part: %r", self.description, part)
+        return True, False
+
+
+LAST_N_DAYS_OF_MONTH_VALIDATOR = PatternValidator(
+    description="Last N days of month",
+    pattern=LAST_N_DAYS_OF_MONTH_PATTERN,
+    validator=lambda part: 1 <= int(part) <= 30,
+    failure_hint="must be between 1 and 30",
+)
+
+NEAREST_WEEKDAY_VALIDATOR = PatternValidator(
+    description="Nearest weekday",
+    pattern=NEAREST_WEEKDAY_DAY_OF_MONTH_PATTERN,
+    validator=lambda part: 1 <= int(part) <= 31,
+    failure_hint="must be between 1 and 31",
+)
+
+
+SINGLE_LITERAL_VALIDATOR = PatternValidator(description="Single literal", pattern=SINGLE_LITERAL_PATTERN)
+WILDCARD_VALIDATOR = PatternValidator(description="Wildcard", pattern=WILDCARD_PATTERN)
+RANGE_VALIDATOR = PatternValidator(description="Range", pattern=RANGE_PATTERN)
+INCREMENT_VALIDATOR = PatternValidator(description="Increment", pattern=INCREMENT_PATTERN)
+RANGE_WITH_INCREMENT_VALIDATOR = PatternValidator(
+    description="Range with increment", pattern=RANGE_WITH_INCREMENT_PATTERN
+)
+LIST_OF_DIGITS_VALIDATOR = PatternValidator(description="List of digits", pattern=LIST_OF_DIGITS_PATTERN)
+LIST_OF_STRINGS_VALIDATOR = PatternValidator(description="List of strings", pattern=LIST_OF_STRINGS_PATTERN)
+DOW_OCCURRENCE_VALIDATOR = PatternValidator(description="Day of week occurrence", pattern=DOW_OCCURRENCE_PATTERN)
+LAST_DOW_IN_MONTH_VALIDATOR = PatternValidator(
+    description="Last day of week in month", pattern=LAST_DOW_IN_MONTH_PATTERN
+)
+DOW_RANGE_VALIDATOR = PatternValidator(description="Day of week range", pattern=DOW_RANGE_PATTERN)
+SPECIFIC_DOW_VALIDATOR = PatternValidator(description="Specific day of week", pattern=SPECIFIC_DOW_PATTERN)
+MONTH_RANGE_VALIDATOR = PatternValidator(description="Month range", pattern=MONTH_RANGE_PATTERN)
+SPECIFIC_MONTH_VALIDATOR = PatternValidator(description="Specific month", pattern=SPECIFIC_MONTH_PATTERN)
+
+
+DEFAULT_NUMERIC_VALIDATORS = (
+    RANGE_VALIDATOR,
+    INCREMENT_VALIDATOR,
+    RANGE_WITH_INCREMENT_VALIDATOR,
+    LIST_OF_DIGITS_VALIDATOR,
+)
 
 
 def validate_day_of_month_or_week(day_of_month: str | int | None, day_of_week: str | int | None) -> None:
@@ -137,7 +241,7 @@ def validate_patterns(part: str, patterns: Sequence[re.Pattern]) -> bool:
 
     for p in patterns:
         if p.match(part):
-            LOGGER.debug("Pattern matched: %s for part: %s", p.pattern, part)
+            LOGGER.debug("Pattern matched: '%s' for part: %r", p.pattern, part)
             return True
 
     return False
@@ -159,7 +263,7 @@ def validate_literals(part: str, literals: set[str]) -> bool:
     lcase_literals = {lit.lower() for lit in literals}
 
     if part.lower() in lcase_literals:
-        LOGGER.debug("Literal matched: %s", part)
+        LOGGER.debug("Literal matched: %r", part)
         return True
 
     return False
